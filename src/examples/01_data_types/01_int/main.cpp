f858f1821118b1ee74d24cb7bcaef42f41374a16
Arya Bayati <82055432+AryByt@users.noreply.github.com>
2024-01-26 01:45:15 +0000

    Hw 1 and examples

--- src/examples/01_data_types/01_int/main.cpp
+++ src/examples/01_data_types/01_int/main.cpp
@@ -1,5 +1,11 @@
+#include "int.h"
+#include<iostream>
+
+using std::cout;
+
 int main()
 {
-
+cout<<"Echo: "<<echo_variable(10)<<"/n";
     return 0;
 }
+//data types, Int- whole num , double- decimal num, char- 'b' character, string - sequence of cahr "ctt"
--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -1,5 +1,5 @@
 #include "data_types.h"
-
+#include "data_types.h"
 //example
 int add_numbers(int num1, int num2)
 {
@@ -8,6 +8,11 @@ int add_numbers(int num1, int num2)
 
 //write function code here
 
-
+int multiply_numbers(int num1)
+{
+	int num2 = 5;
+	int return_value = num1 * num2;
+	return return_value;
+}
 
 
